df44f7d425a2ace1da2df84ed62175f9c7897146
jonastdr <tortato.jonas@gmail.com>
2015-04-17 20:45:08 -0300

    plicas

--- model/cor.php
+++ model/cor.php
@@ -4,7 +4,7 @@ class Cor extends Model {
 
     public function cadastrar($cor)
     {
-        $sql = "INSERT INTO cor (nome) VALUES ({$cor->nome})";
+        $sql = "INSERT INTO cor (nome) VALUES ('{$cor->nome}')";
         
         $this->executar($sql); 
     }
