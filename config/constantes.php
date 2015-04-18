7069a90bb635e5e6279e87bcf4368ec2a64626ce
Lucas Messias <lucasmessias.pg@outlook.com>
2015-04-17 21:13:59 -0300

    ignore

--- config/constantes.php
+++ config/constantes.php
@@ -9,9 +9,11 @@ define('VIEW', 'view');
 //CONSTANTE DE VIEW PARA O SISTEMA
 define('MODEL', 'model');
 
+define('URLBASE', 'http://localhost/concessionaria');
+
 //DEFINICOES PARA O BANCO DE DADOS
 
-if (pg_connect("host=localhost user=postgres password=postgres dbname=veiculos")) {
+if (pg_connect("host=localhost user=postgres password=postgres dbname=concessionaria")) {
 	define('DRIVER', 'postgres');
 	define('HOST', 'localhost');
 	define('USER', 'postgres');
---
+++ view/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Concessionaria</title>
+    <script scr=""></script>
+</head>
+<body>
+    
+</body>
+</html>
