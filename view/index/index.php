9944e5280e8a93d2e026f1aa39ad13acca3f112b
jonastdr <tortato.jonas@gmail.com>
2015-04-30 20:40:19 -0300

    ghdkkdkghkhd

--- view/index/index.php
+++ view/index/index.php
@@ -5,5 +5,6 @@
     <a href="<?php echo URL.'modelo'?>" class="btn btn-default">Modelo</a>
     <a href="<?php echo URL.'cor'?>" class="btn btn-default">Cor</a>
     <a href="<?php echo URL.'veiculo'?>" class="btn btn-default">Veiculo</a>
+    <a href="http://localhost/framework/" class="btn btn-info">Concessionária</a>
 </div>
 <?php $this->render('footer') ?>
